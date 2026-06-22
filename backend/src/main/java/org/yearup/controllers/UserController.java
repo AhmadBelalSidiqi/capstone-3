eb08abf6da937e7adffb868d4b13c6d6cd3a78cd
ahmadbelalsidiqi <Ahmad.sidiqiNe@gmail.com>
2026-06-22 12:57:20 -0400

    UserController class created, getProfile and updatedProfile method added,Http Request of 4.1- 4.5 passed

---
+++ backend/src/main/java/org/yearup/controllers/UserController.java
@@ -0,0 +1,48 @@
+package org.yearup.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.yearup.models.Profile;
+import org.yearup.models.User;
+import org.yearup.service.ProfileService;
+import org.yearup.service.UserService;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/profile")
+@PreAuthorize("hasRole('ROLE_USER')")
+class UserController {
+    private final UserService userService;
+    private final ProfileService profileService;
+
+    @Autowired
+    public UserController(UserService userService, ProfileService profileService) {
+        this.userService = userService;
+        this.profileService = profileService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Profile> profile(Principal principal){
+        String userName = principal.getName();
+        User user = userService.getByUserName(userName);
+        int userId = user.getId();
+        return ResponseEntity.ok(profileService.getProfileByUserId(userId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)));
+    }
+
+    @PutMapping
+    public ResponseEntity<Profile> updateProfile(@RequestBody Profile profile, Principal principal){
+        String userName = principal.getName();
+        User user = userService.getByUserName(userName);
+        int userId = user.getId();
+        return ResponseEntity.ok(profileService.updateProfile(userId,profile));
+    }
+
+
+
+}
--- backend/src/main/java/org/yearup/service/ProfileService.java
+++ backend/src/main/java/org/yearup/service/ProfileService.java
@@ -4,10 +4,16 @@ import org.springframework.stereotype.Service;
 import org.yearup.models.Profile;
 import org.yearup.repository.ProfileRepository;
 
+import java.util.Optional;
+
 @Service
 public class ProfileService
 {
     private final ProfileRepository profileRepository;
+
+    public Optional<Profile> getProfileByUserId(int userId){
+        return profileRepository.findById(userId);
+    }
 
     public ProfileService(ProfileRepository profileRepository)
     {
@@ -17,5 +23,18 @@ public class ProfileService
     public Profile create(Profile profile)
     {
         return profileRepository.save(profile);
+    }
+
+    public Profile updateProfile(int userId, Profile profile) {
+        Profile existingProfile = getProfileByUserId(userId).orElseThrow();
+        existingProfile.setFirstName(profile.getFirstName());
+        existingProfile.setLastName(profile.getLastName());
+        existingProfile.setPhone(profile.getPhone());
+        existingProfile.setEmail(profile.getEmail());
+        existingProfile.setAddress(profile.getAddress());
+        existingProfile.setCity(profile.getCity());
+        existingProfile.setState(profile.getState());
+        existingProfile.setZip(profile.getZip());
+        return profileRepository.save(existingProfile);
     }
 }
